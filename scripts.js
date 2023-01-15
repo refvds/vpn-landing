74582b70ad7908730cb7e64ea5d60dc72f48cb47
refvds <refvdsk@gmail.com>
2023-01-15 20:31:32 +0300

    feat: add responsive price section

--- scripts.js
+++ scripts.js
@@ -33,22 +33,3 @@ function setDigits() {
   updateCount();
  });
 }
-
-function setHoverToCardPrice() {
- priceCards.forEach((card) => {
-  card.addEventListener('mouseover', () => {
-   card.classList.add('price__card--active');
-   const button = card.querySelector('a');
-   button.classList.remove('button--outlined');
-   button.classList.add('button--contained');
-   button.classList.add('button--shadow');
-  });
-  card.addEventListener('mouseout', () => {
-   card.classList.remove('price__card--active');
-   const button = card.querySelector('a');
-   button.classList.add('button--outlined');
-   button.classList.remove('button--contained');
-   button.classList.remove('button--shadow');
-  });
- });
-}
